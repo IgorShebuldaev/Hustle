543865bc313f9bc87fadbeaeab377758f4fca23e
Igor Shebuldaev <igor.shebuldaev@gmail.com>
2020-04-30 17:28:22 +0300

    Initialization of the project.

---
+++ app/src/main/java/org/hustle/MainActivity.java
@@ -0,0 +1,51 @@
+package org.hustle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static org.hustle.utils.NetworkUtils.*;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+
+    class QueryTask extends AsyncTask<URL, Void, String> {
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            String response = null;
+            try {
+                response = getResponseFromURL(urls[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String response) {
+            textView.setText(response);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        try {
+            new QueryTask().execute(getURL());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ app/src/main/java/org/hustle/utils/NetworkUtils.java
@@ -0,0 +1,34 @@
+package org.hustle.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class NetworkUtils {
+    public static URL getURL() throws MalformedURLException {
+        return new URL("http://10.0.2.2:3000/");
+    }
+
+    public static String getResponseFromURL(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+        try {
+            InputStream in = urlConnection.getInputStream();
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+}

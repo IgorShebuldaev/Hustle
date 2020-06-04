d71e763f24dcfb545563aad5cc5bcb2b492794da
Igor Shebuldaev <igor.shebuldaev@gmail.com>
2020-06-04 14:02:44 +0300

    Implement hamburger icon on toolbar navigation.

--- app/src/main/java/org/hustle/MainActivity.java
+++ app/src/main/java/org/hustle/MainActivity.java
@@ -1,7 +1,9 @@
 package org.hustle;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -26,8 +28,10 @@ import java.net.URL;
 import static org.hustle.utils.Network.*;
 
 public class MainActivity extends AppCompatActivity {
-    MyRecyclerViewAdapter adapter;
-    SwipeRefreshLayout refreshLayout;
+    private DrawerLayout drawer;
+    private SwipeRefreshLayout refreshLayout;
+    private MyRecyclerViewAdapter adapter;
+
 
     class QueryTask extends AsyncTask<URL, Void, JSONArray> {
 
@@ -72,8 +76,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.navigation_view);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
         refreshLayout = findViewById(R.id.refreshLayout);
 
@@ -87,6 +96,15 @@ public class MainActivity extends AppCompatActivity {
         });
 
         updateData();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     @Override
